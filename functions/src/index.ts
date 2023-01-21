f7c16986a9ff5c6241fe2c3e2007b145c6aa8a4b
bright0824 <brfuture0824@gmail.com>
2023-01-20 19:55:26 -0700

    fix firebase

---
+++ functions/src/index.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase-admin/app";
+
+initializeApp();
+
+// User functions
+export { clearTab, toggleRole } from "./user/user";
+
+// Item functions
+export { addItem, deleteItem, updateItem } from "./items";
+
+// Auth functions
+export { beforeCreate, onCreate, onDelete } from "./user/auth";
