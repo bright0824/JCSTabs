d23c7072a3c47a8fed37c0297e656e849e9ad3d3
bright0824 <brfuture0824@gmail.com>
2023-01-30 17:32:18 -0700

    add router guards

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,27 @@
 import { createRouter, createWebHistory } from "vue-router/auto";
+import { useFirebaseAuth } from "vuefire";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+});
+
+router.beforeEach(async (to, from) => {
+  const auth = useFirebaseAuth();
+  const { admin } =
+    (await auth?.currentUser
+      ?.getIdTokenResult()
+      .then((idTokenResult) => idTokenResult.claims)) || {};
+
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+  const requiresAdmin = to.matched.some((record) => record.meta.requiresAdmin);
+
+  if (requiresAuth && !auth?.currentUser) {
+    return { name: "login" };
+  }
+
+  if (requiresAdmin && !admin) {
+    return from.path;
+  }
 });
 
 export default router;
